91593f6ffe530dcdafdeb2a971d2639812a9297f
sw25rfWelSIL <welsil34@gmail.com>
2025-05-28 20:58:24 -0300

    Primeiro Commit- Projeto responsavidade

---
+++ script.js
@@ -0,0 +1,24 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector('.imagem-tenis')
+
+
+
+function mudarVisual(cor, imagem){
+
+    tenis.classList.add('troca-efeito')
+
+   
+    body.style.background = cor
+    
+
+    setTimeout(() => {
+
+        tenis.src = imagem
+      tenis.classList.remove('troca-efeito') 
+
+    }, 500);
+
+
+
+   
+}
